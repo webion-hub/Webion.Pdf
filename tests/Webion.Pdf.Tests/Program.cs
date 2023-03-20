1e73824ce3c0faa76b0098b5b7d5055045f45ad4
Stefano Calabretti <stefano.calabretti@webion.it>
2023-03-20 10:21:47 +0100

    Create project

---
+++ tests/Webion.Pdf.Tests/Program.cs
@@ -0,0 +1,7 @@
+﻿using iText.Html2pdf;
+
+using var htmlSource = File.Open("in/input.html", FileMode.Open);
+using var pdfDest = File.Open("out/output.pdf", FileMode.Create);
+
+ConverterProperties converterProperties = new ConverterProperties();
+HtmlConverter.ConvertToPdf(htmlSource, pdfDest, converterProperties);
