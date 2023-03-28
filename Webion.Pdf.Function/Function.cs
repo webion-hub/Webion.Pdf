cf03ffafac9203de9c85403069dced48b9c04845
davidemesso <davide.messori@webion.it>
2023-03-28 11:13:31 +0200

    Changed response to base64

--- Webion.Pdf.Function/Function.cs
+++ Webion.Pdf.Function/Function.cs
@@ -1,9 +1,10 @@
+using System.Text;
 using Google.Cloud.Functions.Framework;
 using Google.Cloud.Functions.Hosting;
 using iText.Html2pdf;
 using iText.Kernel.Pdf;
 using Microsoft.AspNetCore.Http;
-
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace Webion.Pdf.Function;
@@ -23,14 +24,14 @@ public class Function : IHttpFunction
         ConverterProperties converterProperties = new ConverterProperties();
         HtmlConverter.ConvertToPdf(html, sw, converterProperties);
         
-        context.Response.ContentType = "application/pdf"; 
         context.Response.StatusCode = StatusCodes.Status200OK;
 
         using var pdfStream = new MemoryStream(pdfDest.ToArray());
         pdfStream.Position = 0;
         using var reader = new StreamReader(pdfStream);
         
-        await context.Response.BodyWriter.WriteAsync(pdfDest.GetBuffer());
+        var base64 = System.Convert.ToBase64String(pdfDest.GetBuffer());
+        await context.Response.BodyWriter.WriteAsync(Encoding.UTF8.GetBytes(base64));
 
         return;
     }
